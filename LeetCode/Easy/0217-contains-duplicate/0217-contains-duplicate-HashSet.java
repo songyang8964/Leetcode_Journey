1b2747fb89beabba7b8d37bda0c3322d35618906
Song Yang <109130469+songyang8964@users.noreply.github.com>
2025-11-05 23:07:01 +0000

    12 ms (88.67%) 98 MB (5.04%) 11-05-2025 0217-contains-duplicate .java Easy

---
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate-HashSet.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return false;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i< nums.length;i++){
+            if(!set.add(nums[i])){
+                return true;
+            }
+            
+        }return false;
+        
+    }
+}
