0edd3a789978d85accffd43877376b269e22b3c3
Song Yang <109130469+songyang8964@users.noreply.github.com>
2025-11-06 11:20:20 +0000

    4 ms (28.13%) 43.1 MB (8.83%) 11-06-2025 0020-valid-parentheses .java Easy

---
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                Character topChar = stack.pop();
+                if (c == ')' && topChar != '(') {
+                    return false;
+                }
+                if (c == ']' && topChar != '[') {
+                    return false;
+                }
+                if (c == '}' && topChar != '{') {
+                    return false;
+                }
+            }
+        }
+        // 只有stack为空才代表整个字符串匹配成功。如果留有一个括号还是失败
+        return stack.isEmpty();
+    }
+}
