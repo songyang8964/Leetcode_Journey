66155c61cdbf80b6855d61a4bd65473455e0f377
Song Yang <109130469+songyang8964@users.noreply.github.com>
2025-11-25 01:15:06 +0000

    14 ms (62.95%) 46.8 MB (88.19%) 11-25-2025 0347-top-k-frequent-elements .java Medium

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,44 +1,38 @@
 class Solution {
-        private class Freq {
-        int e, freq;
-
-        // 构造器，对成员变量进行初始化
-        public Freq(int e, int freq) {
-            this.e = e;
-            this.freq = freq;
-        }
-    }
-
-        private class FreqComparator implements Comparator<Freq> {
-        @Override
-        public int compare(Freq a, Freq b) {
-            return a.freq - b.freq;
-        }
-    }
-
     public int[] topKFrequent(int[] nums, int k) {
-              TreeMap<Integer, Integer> map = new TreeMap<>();
-        for(int num: nums){
-            if(map.containsKey(num))
-                map.put(num, map.get(num) + 1);
-            else
-                map.put(num, 1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
+        // 根据优先队列进行排序
+        // 我们需要依据元素对应的频率来决定优先级
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                // a在map中对应的映射值大小
+                return map.get(a) - map.get(b);
+            }
+        });
 
-        PriorityQueue<Freq> pq = new PriorityQueue<>(new FreqComparator());
-        for(int key: map.keySet()){
-            if(pq.size() < k)
-                pq.add(new Freq(key, map.get(key)));
-            else if(map.get(key) > pq.peek().freq){
-                pq.remove();
-                pq.add(new Freq(key, map.get(key)));
+        for (int key : map.keySet()) {
+            if (pq.size() < k) {
+                pq.add(key);
+            }
+            // 情况 B：队列满了，需要决定是否替换
+            // pq.getFront(): 获取堆顶元素。
+            // 根据我们在 Freq 中写的 compareTo 逻辑，堆顶元素是“频次最小”的那个。
+            else if (map.get(key) > map.get(pq.peek())) {
+                pq.remove(); // 移除堆顶（踢走最弱的）
+                pq.add(key);
             }
         }
-        int n = pq.size(); // 或者 k
-        int[] ans = new int[n];
+        int n = pq.size();
+        int[] res = new int[n];
+        // 根据频次进行返回
         for (int i = 0; i < n; i++) {
-            ans[i] = pq.remove().e;
+            res[i] = pq.remove();
+
         }
-        return ans;
+        return res;
     }
 }
