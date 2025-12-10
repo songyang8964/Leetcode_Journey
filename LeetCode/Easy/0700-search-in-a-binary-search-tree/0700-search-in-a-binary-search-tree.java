8f3d79b21dc12a47bd10ea31347274ca5253592e
Song Yang <109130469+songyang8964@users.noreply.github.com>
2025-12-10 23:48:58 +0800

    3 ms (1.21%) 47.4 MB (14.63%) 12-10-2025 0700-search-in-a-binary-search-tree .java Easy

---
+++ LeetCode/Easy/0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+        public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.val == val) {
+                return node;
+            }
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return null;
+    }
+}
