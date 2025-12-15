b86b92a6893ab81b0b7c8a52f9d4b3bc8e7928db
Song Yang <109130469+songyang8964@users.noreply.github.com>
2025-12-15 08:31:00 +0000

    0 ms (100%) 47.4 MB (24.59%) 12-15-2025 0538-convert-bst-to-greater-tree .java Medium

---
+++ LeetCode/Medium/0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        traversal(root);
+        return root;
+    }
+
+    private void traversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        traversal(root.right);
+        sum = sum + root.val;
+        root.val = sum;
+        traversal(root.left);
+    }
+}
