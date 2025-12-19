1cbbfef33f6844ec2b354e649f942a2774f9ba96
Song Yang <109130469+songyang8964@users.noreply.github.com>
2025-12-19 17:05:38 +0000

    1 ms (94.52%) 47.1 MB (58.64%) 12-19-2025 0203-remove-linked-list-elements .java Easy

--- LeetCode/Easy/0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ LeetCode/Easy/0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -9,26 +9,24 @@
  * }
  */
 class Solution {
-    public ListNode removeElements(ListNode head, int val) {
-        while(head != null && head.val == val){
-            ListNode delNode = head;
-            head = head.next;
-            delNode.next = null;
-        }
-        if(head == null){
-            return null;
-        }
-        ListNode prev = head;
-        while(prev.next !=null){
-            if(prev.next.val == val){
+public ListNode removeElements(ListNode head, int val) {
+        /*
+        这个 while 循环是解决 removeElements 问题时，不使用虚拟头节点（Dummy Node）方法的标准“起手式
+         */
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+
+        ListNode prev = dummyNode;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                // prev的下一个节点等于要删除的节点
                 ListNode delNode = prev.next;
-                prev.next= delNode.next;
+                prev.next = delNode.next;
                 delNode.next = null;
-            }else{
+            } else {
                 prev = prev.next;
             }
         }
-        return head;
-        
+        return dummyNode.next;
     }
 }
