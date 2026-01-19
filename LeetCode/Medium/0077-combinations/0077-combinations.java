bcada850a0ab20f2deea7f2a3a11327e41a9a489
Song Yang <109130469+songyang8964@users.noreply.github.com>
2026-01-19 12:03:39 +0000

    18 ms (80.63%) 98.2 MB (34.38%) 01-19-2026 0077-combinations .java Medium

--- LeetCode/Medium/0077-combinations/0077-combinations.java
+++ LeetCode/Medium/0077-combinations/0077-combinations.java
@@ -1,23 +1,23 @@
 class Solution {
-        List<List<Integer>> results = new ArrayList<>();
+    List<List<Integer>> res = new ArrayList<>();
+
     List<Integer> path = new ArrayList<>();
+
     public List<List<Integer>> combine(int n, int k) {
-        backtracking(n, k ,1);
-        return results;
-
-
+        backtracking(n, k, 1);
+        return res;
     }
 
-    public void backtracking(int n, int k, int startIndex) {
-        if(path.size() == k){
-            results.add(new ArrayList<>(path));
+    private void backtracking(int n, int k, int startIndex) {
+        if (path.size() == k) {
+            res.add(new ArrayList<>(path));
             return;
         }
+
         for (int i = startIndex; i <= n; i++) {
             path.add(i);
-            backtracking(n,k,i+1);
+            backtracking(n, k, i + 1);
             path.remove(path.size() - 1);
-
         }
     }
 }
