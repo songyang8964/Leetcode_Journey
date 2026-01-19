994a134426abdd5ac55d4f1a77af06ac5d332c74
Song Yang <109130469+songyang8964@users.noreply.github.com>
2026-01-19 23:45:17 +0000

    0 ms (100%) 42.6 MB (20.33%) 01-19-2026 0216-combination-sum-iii .java Medium

--- LeetCode/Medium/0216-combination-sum-iii/0216-combination-sum-iii.java
+++ LeetCode/Medium/0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,27 +1,31 @@
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
     List<Integer> path = new ArrayList<>();
+    int sum = 0;
+
     public List<List<Integer>> combinationSum3(int k, int n) {
-        backtracking(n, k , 0 , 1);
+        backtracking(n, k, 1);
         return result;
+
     }
-        void backtracking(int targetSum, int k, int sum, int startIndex){
-        if(sum > targetSum){
+
+    private void backtracking(int n, int k, int startIndex) {
+        if (sum > n) {
             return;
         }
-        if(path.size() == k){
-            if(sum == targetSum){
+        if (path.size() == k) {
+            if (sum == n) {
                 result.add(new ArrayList<>(path));
-                return;
             }
+            return;
         }
-        for (int i = startIndex; i <= 9 - (k - path.size()) + 1 ; i++) {
-            sum = sum + i;
+        int Needed = k - path.size();
+        for (int i = startIndex; i <= 9 - Needed + 1; i++) {
             path.add(i);
-            backtracking(targetSum, k, sum, i+1);
+            sum = sum + i;
+            backtracking(n, k, i + 1);
             sum = sum - i;
             path.remove(path.size() - 1);
-
         }
     }
 }
