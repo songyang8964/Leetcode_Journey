8f31cb0908ad30a27739873de7a266c36ab0ddd5
Song Yang <109130469+songyang8964@users.noreply.github.com>
2026-02-05 21:51:57 +0000

    3 ms (37.97%) 45.8 MB (66.24%) 02-05-2026 0039-combination-sum .java Medium

--- LeetCode/Medium/0039-combination-sum/0039-combination-sum.java
+++ LeetCode/Medium/0039-combination-sum/0039-combination-sum.java
@@ -1,37 +1,29 @@
 class Solution {
-
     List<List<Integer>> res = new ArrayList<>();
     List<Integer> path = new ArrayList<>();
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        // 为了优化（剪枝），先对数组进行排序
-        // 排序后，如果加一个数这就超标了，那后面更大的数肯定也超标，可以直接停止
         Arrays.sort(candidates);
-        // 2. 开启递归
-        // 参数解释：数组，目标值，当前累加和(0)，开始索引(0)
-        backtrack(candidates, target, 0, 0);
-
+        backtrack(candidates, target, 0);
         return res;
     }
 
-    private void backtrack(int[] candidates, int target, int sum, int startIndex) {
-        // edge case
-        if (sum == target) {
+    private void backtrack(int[] candidates, int target, int startIndex) {
+        // 找到答案
+        if (target == 0) {
             res.add(new ArrayList<>(path));
             return;
         }
-        if (sum > target) {
-            return;
-        }
+
         for (int i = startIndex; i < candidates.length; i++) {
-            if (sum + candidates[i] <= target) {
-                path.add(candidates[i]);
-                sum += candidates[i];
-                // 可以重复选取，所以这里startIndex = i
-                backtrack(candidates, target, sum, i);
-                sum -= candidates[i];
-                path.remove(path.size() - 1);
+            // 剪枝：如果当前数已经大于 target，后面更大的数也不用看了
+            if (candidates[i] > target) {
+                break;
             }
+
+            path.add(candidates[i]);
+            backtrack(candidates, target - candidates[i], i);  // target 减小
+            path.remove(path.size() - 1);
         }
     }
 }
