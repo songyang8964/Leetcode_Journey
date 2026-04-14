7ce667c70edb7b84fe8b3750e2a361a5f7f558cf
Song Yang <109130469+songyang8964@users.noreply.github.com>
2026-04-14 15:24:27 +0100

    44 ms (95.53%) 23 MB (8.2%) 04-14-2026 0141-linked-list-cycle .py Easy

---
+++ LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+
+        current = head
+
+        while current:
+            if current in visited:
+                return True
+            else :
+                visited.add(current)
+                current = current.next
+            
+        return False
+        
