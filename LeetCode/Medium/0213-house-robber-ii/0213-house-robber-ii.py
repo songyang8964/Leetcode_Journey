7de667e8907b3f56a67dbb4a05d1f235818822a8
Song Yang <109130469+songyang8964@users.noreply.github.com>
2026-04-16 17:28:11 +0100

    0 ms (100%) 19.2 MB (94.31%) 04-16-2026 0213-house-robber-ii .py Medium

---
+++ LeetCode/Medium/0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    
+    def rob_linear(self, nums: List[int], start: int, end: int) -> int:
+        n = end - start + 1
+        dp = [0] * n
+        dp[0] = nums[start]
+        dp[1] = max(nums[start], nums[start + 1])
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[start + i])
+        return dp[-1]
+    
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        
+        case_B = self.rob_linear(nums, 0, len(nums) - 2)
+        case_C = self.rob_linear(nums, 1, len(nums) - 1)
+        
+        return max(case_B, case_C)
