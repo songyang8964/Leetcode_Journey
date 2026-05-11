eb8330179e75d7b1d91f551aef8c8c6e2afc7532
Song Yang <109130469+songyang8964@users.noreply.github.com>
2026-05-11 07:44:27 +0000

    269 ms (5.07%) 33.2 MB (5.18%) 05-11-2026 0121-best-time-to-buy-and-sell-stock .py Easy

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,23 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
-        if len(prices) <= 1:
-            return 0
-
-        left = 0    # 买入日
-        right = 1   # 卖出日
-        max_profit = 0
-
-        while right < len(prices):
-
-            if prices[right] > prices[left]:
-                # 今天卖出有利润，计算并更新
-                profit = prices[right] - prices[left]
-                max_profit = max(max_profit, profit)
-            else:
-                # 今天价格更低，换成更便宜的买入点
-                left = right
-
-            right += 1
-
-        return max_profit
+        n = len(prices)
+        dp = [[0] * 2 for _ in range(n + 1)]
+        dp[0][0] = 0
+        dp[0][1] = -prices[0]
+        for i in range(1, n+1):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1])
+            dp[i][1] = max(dp[i - 1][1], - prices[i - 1])
+        return dp[n][0]
