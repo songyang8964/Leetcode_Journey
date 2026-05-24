22af4fe4599a5ac0b40d75383e3fecde14207b9c
Song Yang <109130469+songyang8964@users.noreply.github.com>
2026-05-24 14:47:37 +0000

    3 ms (45.31%) 20.4 MB (78.54%) 05-24-2026 0026-remove-duplicates-from-sorted-array .py Easy

--- LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,12 +4,11 @@ class Solution:
             return 0
         if len(nums) == 1:
             return 1
-        
-        slow = 1
-        for fast in range(1,len(nums)):
-            if nums[fast - 1] != nums[fast]:
+        fast = 0
+        slow = 0
+        for fast in range(0, len(nums)):
+            if nums[slow] != nums[fast]:
+                slow += 1
                 nums[slow] = nums[fast]
-                slow = slow + 1
-                
-        return slow
-                
+        return slow + 1
+        
