d6d899371df8aae9f05ad0860cb64370d7db6c53
Song Yang <109130469+songyang8964@users.noreply.github.com>
2026-05-25 09:14:07 +0000

    0 ms (100%) 20.6 MB (19.2%) 05-25-2026 0001-two-sum .py Easy

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -1,11 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
-        for i in range(len(nums)):
-            x = nums[i]
-            complement = target - x
+        for i, num in enumerate(nums):
+            complement = target - num
             if complement in seen:
-                return [seen[complement], i]
-            seen[x] = i
-        return []
+                return[seen[complement], i]
+            seen[num] = i
         
