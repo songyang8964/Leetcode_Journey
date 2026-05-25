624861d7695be10adb82963be480dbb0fb881927
Song Yang <109130469+songyang8964@users.noreply.github.com>
2026-05-25 10:59:41 +0000

    11 ms (33.55%) 23.3 MB (13.03%) 05-25-2026 0125-valid-palindrome .py Easy

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,15 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        left = 0
-        right = len(s) - 1
-        while left <= right:
-            while left < right and not s[left].isalnum():
-                left = left + 1
-            while left < right and not s[right].isalnum():
-                right = right - 1
+        filter =[]
+        for c in s:
+            if c.isalnum():
+                filter.append(c.lower())
 
-            if s[left].lower() != s[right].lower():
+        s = ''.join(filter)
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
                 return False
-            left = left + 1
-            right = right - 1
+            left += 1
+            right -= 1
         return True
+        
