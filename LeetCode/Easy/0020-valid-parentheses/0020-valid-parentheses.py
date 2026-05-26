54cfca4ab3c1b8181bd569ff1b983971d106be76
Song Yang <109130469+songyang8964@users.noreply.github.com>
2026-05-26 15:27:33 +0000

    0 ms (100%) 19.2 MB (67.33%) 05-26-2026 0020-valid-parentheses .py Easy

---
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for char in s:
+            if char == '(' or char == '[' or char == '{':
+                stack.append(char)
+            elif char == ')':
+                if len(stack) == 0 or stack[-1] != '(':
+                    return False
+                stack.pop()
+            elif char == ']':
+                if len(stack) == 0 or stack[-1] != '[':
+                    return False
+                stack.pop()
+            elif char == '}':
+                if len(stack) == 0 or stack[-1] != '{':
+                    return False
+                stack.pop()
+        return len(stack) == 0
+
+        
