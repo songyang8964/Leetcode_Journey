2abc1ac8d6af33eeb523f31e21179743526bb776
Song Yang <109130469+songyang8964@users.noreply.github.com>
2026-05-27 09:27:53 +0000

    0 ms (100%) 19.2 MB (81.26%) 05-27-2026 0349-intersection-of-two-arrays .py Easy

--- LeetCode/Easy/0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ LeetCode/Easy/0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,4 +1,5 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # Python 的 set 支持直接用 & 运算符求交集
         return list(set(nums1) & set(nums2))
         
