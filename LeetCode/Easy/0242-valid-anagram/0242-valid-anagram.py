1b7016d8ff6dad12c8a531586e18f0c994b90133
Song Yang <109130469+songyang8964@users.noreply.github.com>
2026-05-27 09:17:33 +0000

    11 ms (76.73%) 19.5 MB (43.83%) 05-27-2026 0242-valid-anagram .py Easy

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
-        
+        if len(s) != len(t):
+            return False
+        map = {}
+        for i in s:
+            map[i] = map.get(i, 0) + 1
+
+        for j in t:
+            if j not in map:
+                return False
+            else:
+                map[j] -= 1
+        for v in map.values():
+            if v != 0:
+                return False
+        return True
